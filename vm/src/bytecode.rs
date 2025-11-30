07ef0ac476112105811755de057caaedf3f8eb88
Alec Davis <unlikelytitan@gmail.com>
2025-11-29 16:46:24 -0800

    working on conversion between enum and bytes

--- vm/src/bytecode.rs
+++ vm/src/bytecode.rs
@@ -17,14 +17,35 @@ pub enum Target {
     Inter,
 }
 
+impl Target {
+    pub fn to_bytes(&self) -> Vec<u8> {
+        match self {
+            Target::Byte(b) => vec![0x00, *b],
+            Target::Short(b) => vec![0x01, *b],
+            Target::Integer(b) => vec![0x02, *b],
+            Target::Long(b) => vec![0x03, *b],
+            Target::Float(b) => vec![0x04, *b],
+            Target::Double(b) => vec![0x05, *b],
+            Target::Zero => vec![0x06],
+            Target::Seed => vec![0x07],
+            Target::IntExn => vec![0x08],
+            Target::FloatExn => vec![0x09],
+            Target::Trap => vec![0x0a],
+            Target::TrapCause => vec![0x0b],
+            Target::TrapVal => vec![0x0c],
+            Target::Inter => vec![0x0d],
+        }
+    }
+}
+
 pub enum Bytecode {
-    LoadImmediate8(u8),
-    LoadImmediate16(u16),
-    LoadImmediate32(u32),
-    LoadImmediate64(u64),
+    LoadImmediate8(Target, u8),
+    LoadImmediate16(Target, u16),
+    LoadImmediate32(Target, u32),
+    LoadImmediate64(Target, u64),
     LoadRegister(Target, Target),
     LoadAddress(Target, Target),
-    StoreRegister64(Target, Target),
+    StoreRegister(Target, Target),
     AddSigned(Target, Target, Target),
     AddUnsigned(Target, Target, Target),
     SubSigned(Target, Target, Target),
@@ -79,4 +100,53 @@ pub enum Bytecode {
     CardCount(Target),
     CardName(Target, Target),
     CardCall,
+}
+
+impl Bytecode {
+    pub fn to_bytes(&self) -> Vec<u8> {
+        match self {
+            Bytecode::LoadImmediate8(target, b) => {
+                let mut bytes = vec![0x00, 0x00];
+                bytes.extend(target.to_bytes());
+                bytes.push(*b);
+                bytes
+            }
+            Bytecode::LoadImmediate16(target, short) => {
+                let mut bytes = vec![0x01, 0x00];
+                bytes.extend(target.to_bytes());
+                bytes.extend(&short.to_le_bytes());
+                bytes
+            }
+            Bytecode::LoadImmediate32(target, int) => {
+                let mut bytes = vec![0x02, 0x00];
+                bytes.extend(target.to_bytes());
+                bytes.extend(&int.to_le_bytes());
+                bytes
+            }
+            Bytecode::LoadImmediate64(target, long) => {
+                let mut bytes = vec![0x03, 0x00];
+                bytes.extend(target.to_bytes());
+                bytes.extend(&long.to_le_bytes());
+                bytes
+            }
+            Bytecode::LoadRegister(target, src) => {
+                let mut bytes = vec![0x04, 0x00];
+                bytes.extend(target.to_bytes());
+                bytes.extend(src.to_bytes());
+                bytes
+            }
+            Bytecode::LoadAddress(target, src) => {
+                let mut bytes = vec![0x05, 0x00];
+                bytes.extend(target.to_bytes());
+                bytes.extend(src.to_bytes());
+                bytes
+            }
+            Bytecode::StoreRegister(dest, src) => {
+                let mut bytes = vec![0x06, 0x00];
+                bytes.extend(dest.to_bytes());
+                bytes.extend(src.to_bytes());
+                bytes
+            }
+        }
+    }
 }
